cc233659359ad2e6c52308b2db7fafd8ec9ce87b
HeyJude5 <skuwuw456@naver.com>
2025-07-10 10:32:53 +0900

    스트리밍페이지 UI 임시본 추가완료

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -8,10 +8,12 @@ import CreateLecturePage from '@/components/pages/lecture';
 import TokenPage from '@/components/pages/token';
 import { OAuthFinalizePage } from '@/components/pages/oauth/finalize';
 import VerifyEmailPage from '@/components/pages/verifyEmail';
+import StreamingPage from '@/components/pages/streamingpage';
 
 export const AppRoutes = () => {
   return (
     <Routes>
+      {/* App 레이아웃 아래에 들어가는 기존 라우트들 */}
       <Route path="/" element={<App />}>
         <Route index element={<MainPage />} />
         <Route path="register" element={<RegisterPage />} />
@@ -22,6 +24,8 @@ export const AppRoutes = () => {
         <Route path="oauth/finalize" element={<OAuthFinalizePage />} />
         <Route path="verifyEmail" element={<VerifyEmailPage />} />
       </Route>
+      {/* layout(App) 없이, 단독으로 streamingpage 라우트 */}
+      <Route path="/streamingpage" element={<StreamingPage />} />
     </Routes>
   );
 };
