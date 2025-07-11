98bf4b5eaeeaec508add70ed3d99c2d7ac375d6b
Kim Jinsoo <kjs981021@gmail.com>
2025-07-11 13:02:13 +0900

    fix: 배포를 위한 vite 설정 정리

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,16 +5,13 @@ import path from 'path';
 
 export default defineConfig({
   base: './',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    proxy: {
-      '/auth': 'http://localhost:5000',
-      '/lectures': 'http://localhost:5000', // ← 이 줄 추가!
     },
   },
 });
