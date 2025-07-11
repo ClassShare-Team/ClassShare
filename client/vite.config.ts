255a936052cc5b5966ab9ee67e16d7a002a1a58c
Kim Jinsoo <kjs981021@gmail.com>
2025-07-11 13:19:45 +0900

    fix: redirects가 빌드 결과물에 포합되도록 수동 설정

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,11 +2,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import fs from 'fs';
 
 export default defineConfig({
   base: './',
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      plugins: [
+        {
+          name: 'copy-redirects',
+          writeBundle() {
+            fs.copyFileSync(
+              path.resolve(__dirname, 'public/_redirects'),
+              path.resolve(__dirname, 'dis/_redirects')
+            );
+          },
+        },
+      ],
+    },
   },
   plugins: [react()],
   resolve: {
