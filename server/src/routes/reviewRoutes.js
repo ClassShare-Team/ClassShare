5e09cf4609ab8e9cafcbda6f09cebc00407d5dde
ch0chuu <gegurl1187@gmail.com>
2025-07-16 19:23:03 +0900

    fix: authMiddleware 추가

--- server/src/routes/reviewRoutes.js
+++ server/src/routes/reviewRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const reviewController = require('../controllers/reviewController');
-const authMiddleware = require('../middleware/authMiddleware');
+const { authMiddleware } = require('../middleware/authMiddleware');
 
-router.post('/', authMiddleware.verifyToken, reviewController.create);
+router.post('/', authMiddleware, reviewController.create);
 router.post('/reviews/:reviewId/comment', authMiddleware, reviewController.replyToReview);
 
 module.exports = router;
